613ac27954f09b8f639c49f00f993a5a91c4395d
RedRex100 <129302477+RedRex100@users.noreply.github.com>
2024-10-11 11:34:23 -0300

    Update app.py

--- projeto/app.py
+++ projeto/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
+from models import Usuarios
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def render1():
+    return render_template("pm.html")
 
-@app.route("/secret")
-def secret():
-    return render_template('secreto.html')
+@app.route("/login")
+def render2():
+    return render_template("index.html")
 
-if __name__ == ("__main__"):  
+if __name__ == "__main__":
     app.run(debug=True)
-
