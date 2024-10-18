94bf750d87290748f2c8f908ca49b5879e8b64d3
RedRex100 <129302477+RedRex100@users.noreply.github.com>
2024-10-17 21:11:57 -0300

    Delete projeto directory

--- projeto/models/modelo.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self, login, senha):
-        self.login = login
-        self.senha = senha
-Usuarios = []
-
-def adicionar_usuario(login, senha):
-    novo_usuario = Usuario(login, senha)
-    Usuarios.append(novo_usuario)
