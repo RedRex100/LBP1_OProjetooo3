db512e9447683c72199db941a45ee48933329b26
RedRex100 <129302477+RedRex100@users.noreply.github.com>
2024-11-01 09:28:27 -0300

    Add files via upload

---
+++ Projeto/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from controllers.cadastro import adicionar, Usuarios
+
+app = Flask(__name__)
+app.secret_key = 'senha ultra secreta'
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+        adicionar(nome, senha)
+        return redirect(url_for('login'))
+    return render_template('cadastro.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+        for usuario in Usuarios:
+            if usuario.nome == nome and usuario.senha == senha:
+                session['usuario'] = nome  # Armazena o nome do usuário na sessão
+                return redirect(url_for('dashboard'))
+    return render_template('login.html')
+        
+    flash('Nome ou senha incorretos!')  # Mensagem de erro se as credenciais estiverem erradas
+
+@app.route('/dashboard')
+def dashboard():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    return f'Bem-vindo, {session["usuario"]}!'
+
+@app.route('/loja')
+def loja():
+    return render_template('loja.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)  # Remove o usuário da sessão
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+     app.run(debug = True)
---
+++ Projeto/controllers/cadastro.py
@@ -0,0 +1,23 @@
+from flask import session, flash, request, redirect, url_for, render_template
+from models.model import Usuario, Usuarios
+
+def adicionar(nome, senha):
+    novo_usaurio = Usuario(nome, senha)
+    Usuarios.append(novo_usaurio)
+
+def login():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+        for usuario in Usuarios:
+            if usuario.nome == nome and usuario.senha == senha:
+                session['usuario'] = nome  # Armazena o nome do usuário na sessão
+                return redirect(url_for('dashboard'))
+    return render_template('login.html')
+        
+    flash('Nome ou senha incorretos!')  
+    # Mensagem de erro se as credenciais estiverem erradas
+
+def deslogar():
+    session.pop('usuario', None)
+    return redirect(url_for('login'))
